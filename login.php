8f72e607e89d52793160e27b8a284f4d99b36ea9
devcoder2000 <web.coder.2000@gmail.com>
2022-11-03 22:29:50 -0600

    Migrando a php

--- login.php
+++ login.php
@@ -1,39 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-
-    <link
-      rel="shortcut icon"
-      href="img/Logo-Estudio-Contable.png"
-      type="image/x-icon"
-    />
-    <title>Login</title>
-  </head>
-  <body>
-    <div class="divContainer__h">
-      <a class="divContainer__h__logo" href="index.html">
-        <img src="img/Logo-Estudio-Contable.png" alt="Logo EC" />
-
-        <p>Estudio Contable - Torres Asociados</p>
-      </a>
-
-      <nav class="divContainer__h__nav">
-        <ul>
-          <li><a href="index.html">Inicio</a></li>
-          <li><a href="">Noticias</a></li>
-          <li><a href="">Servicios</a></li>
-          <li><a href="">Clientes</a></li>
-          <li><a href="">Contactanos</a></li>
-          <li><a href="login.html">Login</a></li>
-        </ul>
-      </nav>
-    </div>
-    <hr />
-    <div class="form">
+<?php include('templates/head.php');?>
+<div class="form">
       <div class="form__title">
         <img src="/img/Logo-Estudio-Contable.png" alt="" />
         <h1>Iniciar Sesion</h1>
@@ -85,5 +51,3 @@
         <p>© Copyright DevCoder 2022. Reservados todos los derechos.</p>
       </div>
     </div>
-  </body>
-</html>
